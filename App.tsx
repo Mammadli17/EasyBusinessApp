cb8a83d41af29b564782e605685c8fb07171a478
Mammadli17 <130707388+Mammadli17@users.noreply.github.com>
2025-04-09 01:05:27 +0400

    commit

--- App.tsx
+++ App.tsx
@@ -1,9 +1,10 @@
-import { View, Text } from 'react-native'
+import { View, Text, StatusBar } from 'react-native'
 import React from 'react'
 import Router from './src/navigations/Router'
 const App = () => {
   return (
     <View style={{ flex: 1 }}>
+      <StatusBar backgroundColor='transparent'/>
       <Router />
     </View>
   )
